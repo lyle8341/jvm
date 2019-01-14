fd4a8f81568a6774af304a4f1abf6cbada5cfa90
lyle8341 <lyllyl0000@126.com>
2019-01-14 23:53:40 +0800

    init

---
+++ src/test/java/com/lyle/jvm/JvmApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lyle.jvm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JvmApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
